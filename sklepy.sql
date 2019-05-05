58723ed295c5995e2f5cf8ca741fe86ecb19d254
Talar1997 <32823145+Talar1997@users.noreply.github.com>
2019-05-05 22:42:16 +0200

    Update sklepy.sql

--- sklepy.sql
+++ sklepy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Kwi 2019, 18:35
+-- Czas generowania: 05 Maj 2019, 22:41
 -- Wersja serwera: 10.1.26-MariaDB
 -- Wersja PHP: 7.1.8
 
@@ -21,6 +21,32 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `sklepy`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `markers`
+--
+
+CREATE TABLE `markers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) COLLATE utf8_polish_ci NOT NULL,
+  `address` varchar(80) COLLATE utf8_polish_ci NOT NULL,
+  `lat` float(10,6) NOT NULL,
+  `lng` float(10,6) NOT NULL,
+  `type` varchar(30) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `markers`
+--
+
+INSERT INTO `markers` (`id`, `name`, `address`, `lat`, `lng`, `type`) VALUES
+(1, 'Uniwersytet Śląski', 'ul. Będzińska, Sosnowiec', 50.288738, 19.129940, 'restaurant'),
+(2, 'Lorem Ipsum ', 'ul. Będzińska, Sosnowiec', 50.287739, 19.128941, 'bar'),
+(3, 'Lorem Ipsum ', 'ul. Będzińska, Sosnowiec', 50.287739, 18.128941, 'bar'),
+(4, 'Lorem Ipsum ', 'ul. Będzińska, Sosnowiec', 51.287739, 19.128941, 'bar'),
+(5, 'Lorem Ipsum ', 'ul. Będzińska, Sosnowiec', 52.287739, 23.128941, 'bar');
 
 -- --------------------------------------------------------
 
@@ -45,11 +71,18 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `login`, `password`, `role`, `security_question`, `security_answer`, `email`) VALUES
 (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', '2+2', 'a87ff679a2f3e71d9181a67b7542122c', 'admin@admin.pl'),
 (4, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user', '2x10', '98f13708210194c475687be6106a3b84', 'user@user.pl'),
-(5, 'Consuch', 'a1e03cf022862126dabb2b06e3223521', 'user', 'Nazwa serwera', '421aa90e079fa326b6494f812ad13e79', 'Consuch@interia.pl');
+(5, 'Consuch', 'a1e03cf022862126dabb2b06e3223521', 'user', 'Nazwa serwera', '421aa90e079fa326b6494f812ad13e79', 'Consuch@interia.pl'),
+(9, 'Talar', 'a1e03cf022862126dabb2b06e3223521', 'user', 'Nazwa serwera na MTA', 'bd0f1baa6a3a2db9e53973e9650197d6', 'Adam.Talarczyk@hotmail.com');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indexes for table `markers`
+--
+ALTER TABLE `markers`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -62,10 +95,15 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `markers`
+--
+ALTER TABLE `markers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
 -- AUTO_INCREMENT dla tabeli `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
